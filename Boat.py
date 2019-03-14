ea7a82124078ebf75edcd2587ea93e953c8d8ea6
Fayazov <afayazov@plazius.ru>
2019-03-14 22:26:02 +0300

    Move new Sprite class to file

--- Boat.py
+++ Boat.py
@@ -1,43 +1,9 @@
 import cocos
-import pyglet
 
 from KeyHandler import KeyHandler
+from Sprite import Sprite
 from settings import WINDOW_WIDTH, WINDOW_HEIGHT
 
-class Sprite(cocos.sprite.Sprite):
-    def __init__(self, *args):
-        super(Sprite, self).__init__(*args)
-
-    def do(self, action, target=None):
-        """Executes an :class:`.Action`.
-        When the action is finished, it will be removed from the node's actions
-        container.
-
-        To remove an action you must use the :meth:`do` return value to
-        call :meth:`remove_action`.
-
-        Arguments:
-            action (Action):
-                Action that will be executed.
-        Returns:
-            Action: A clone of ``action``
-
-        """
-        a = action
-
-        if target is None:
-            a.target = self
-        else:
-            a.target = target
-
-        a.start()
-        self.actions.append(a)
-
-        if not self.scheduled:
-            if self.is_running:
-                self.scheduled = True
-                pyglet.clock.schedule(self._step)
-        return a
 
 class Mover(cocos.actions.Move):
     def __init__(self, vel):
@@ -80,4 +46,3 @@ class Boat(KeyHandler, cocos.layer.Layer):
 
     def go_down(self):
         self.vel['y'] -= self.speed
-
---
+++ Sprite.py
@@ -0,0 +1,38 @@
+import cocos
+import pyglet
+
+
+class Sprite(cocos.sprite.Sprite):
+    def __init__(self, *args):
+        super(Sprite, self).__init__(*args)
+
+    def do(self, action, target=None):
+        """Executes an :class:`.Action`.
+        When the action is finished, it will be removed from the node's actions
+        container.
+
+        To remove an action you must use the :meth:`do` return value to
+        call :meth:`remove_action`.
+
+        Arguments:
+            action (Action):
+                Action that will be executed.
+        Returns:
+            Action: A clone of ``action``
+
+        """
+        a = action
+
+        if target is None:
+            a.target = self
+        else:
+            a.target = target
+
+        a.start()
+        self.actions.append(a)
+
+        if not self.scheduled:
+            if self.is_running:
+                self.scheduled = True
+                pyglet.clock.schedule(self._step)
+        return a
